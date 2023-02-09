d6003b4ee77d0659f43df245c3f0142e95bce914
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-02-09 18:01:58 +0100

    change

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 // tady je místo pro náš program
+let jmeno = prompt('Jaké je tvoje jméno?')
 
+let prijmeni = prompt('Jaké je tvoje příjmení?')
 
+let vek = prompt ('Zadej rok svého narození.')
+
+let barva = prompt('Jaká je tvoje oblíbená barva (prosím, uveď ji v angličtině?)')
+function zmenaBarvy () {
+    document.querySelector.innerHTML = 
+    document.body.style = barva;
+}
+    
+document.body.innerHTML = jmeno + ' ' + prijmeni + ' ' + vek
+
+/*
+let barva = prompt('Jaká je tvoje oblíbená barva (prosím, uveď ji v angličtině?')
+*/
 
 
